885dd34ea77205d6d3c5164b848cc272873d0081
ShotaTogawa <s.togawa0112@gmail.com>
2020-05-11 15:01:53 +0900

    connected redux

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodo } from '../actions';
+import { StoreState } from '../reducers';
 
-class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
   render() {
     return <div>Hi there</div>;
   }
 }
 
-export default App;
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+// export defaultを使わない
+export const App = connect(mapStateToProps, { fetchTodo })(_App);
